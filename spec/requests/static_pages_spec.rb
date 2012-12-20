99adbf35a7c64e957a9befae82294ae947469530
Dilshod Saidov <dilshod.saidov@gmail.com>
2012-12-20 09:42:49 -0500

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,42 +1,3 @@
-=begin
-require 'spec_helper'
-
-describe "Static pages" do
-
-  subject { page }
-
-  describe "Home page" do
-    before { visit root_path }
-
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
-  end
-
-  describe "Help page" do
-    before { visit help_path }
-
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
-  end
-
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
-  end
-end
-
-=end
-
 require 'spec_helper'
 
 describe "Static pages" do
@@ -70,6 +31,30 @@ describe "Static pages" do
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+
+      describe "should have microposts count" do
+        it { should have_content("2 microposts") }
+      end
+
+      describe "pagination" do
+
+        before(:all) { 30.times { FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum") } }
+        after(:all)  { User.delete_all }
+
+        it { should have_selector('div.pagination') }
+      end
+
     end
 
   end
